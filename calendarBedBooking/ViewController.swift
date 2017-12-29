c9b8925a6d26ebd73e15f86a320fdac9e75d91e3
ekorszaczuk <e.korszaczuk@goapps.pl>
2017-12-29 09:32:01 +0100

    Initial Commit

---
+++ calendarBedBooking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calendarBedBooking
+//
+//  Created by ekorszaczuk on 29.12.2017.
+//  Copyright © 2017 ekorszaczuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
